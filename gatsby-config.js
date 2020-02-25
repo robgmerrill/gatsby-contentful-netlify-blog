3c3ab570e68140e89a4aeabf1eae3d94110ab09a
Robert Merrill <robgmerrill@gmail.com>
2020-02-25 11:14:10 -0800

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Awesome Blog`,
+    description: `An awesome blog displaying my awesome posts.`,
+    author: `YOUR_NAME`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +25,13 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `qjeloukq2c3i`,
+        accessToken: `MkEpYbVVCRRTnbEMM1B349vR8R2UQ1seArnGTrt5nDM`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
